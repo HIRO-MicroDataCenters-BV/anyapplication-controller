feca5b36b848d28632d7d6584496e7aedd86980b
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-05-06 11:35:08 +0200

    [reconciler] example of the application

--- internal/controller/types/job_conditions.go
+++ internal/controller/types/job_conditions.go
@@ -1,21 +1,36 @@
 package types
 
 import (
+	"github.com/samber/mo"
 	v1 "hiro.io/anyapplication/api/v1"
 )
 
-type JobApplicationConditions struct {
-	Conditions []*v1.ConditionStatus
+type JobApplicationCondition struct {
+	condition mo.Option[v1.ConditionStatus]
+	jobType   AsyncJobType
 }
 
-func EmptyJobConditions() JobApplicationConditions {
-	return JobApplicationConditions{}
+func EmptyJobConditions() JobApplicationCondition {
+	return JobApplicationCondition{}
 }
 
-func FromCondition(condition *v1.ConditionStatus) JobApplicationConditions {
-	return JobApplicationConditions{
-		Conditions: []*v1.ConditionStatus{
-			condition,
-		},
+func FromCondition(condition v1.ConditionStatus) JobApplicationCondition {
+	return JobApplicationCondition{
+		condition: mo.Some(condition),
+	}
+}
+
+func (j JobApplicationCondition) GetConditions() []*v1.ConditionStatus {
+	if j.condition.IsAbsent() {
+		return nil
+	} else {
+		return []*v1.ConditionStatus{j.condition.ToPointer()}
+	}
+}
+func (j JobApplicationCondition) GetJobType() mo.Option[AsyncJobType] {
+	if j.condition.IsAbsent() {
+		return mo.None[AsyncJobType]()
+	} else {
+		return mo.Some(j.jobType)
 	}
 }
