8cda4567742ebf6026cfaf964983f69cd99c4b96
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-07-14 15:58:02 +0200

    [failures] events and logs tests

--- internal/errorctx/logs_test.go
+++ internal/errorctx/logs_test.go
@@ -2,10 +2,13 @@ package errorctx
 
 import (
 	"context"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	k8sfake "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/testing"
@@ -17,15 +20,31 @@ var _ = Describe("RealLogFetcher", func() {
 		fetcher    *RealLogFetcher
 		ctx        context.Context
 		logData    = "fake logs"
+		eventData  *corev1.EventList
 	)
 
 	BeforeEach(func() {
 		ctx = context.Background()
 		fakeClient = k8sfake.NewSimpleClientset()
+		eventData = &corev1.EventList{
+			Items: []corev1.Event{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-event",
+						Namespace: "default",
+					},
+					Reason:  "TestReason",
+					Message: "This is a test event",
+				},
+			},
+		}
 
-		// Inject log stream mock into the reactor
 		fakeClient.Fake.PrependReactor("get", "pods", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
 			return false, nil, nil // Allow GetLogs to continue
+		})
+
+		fakeClient.Fake.PrependReactor("list", "events", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+			return true, eventData, nil
 		})
 
 		fetcher = NewRealLogFetcher(fakeClient)
@@ -37,4 +56,13 @@ var _ = Describe("RealLogFetcher", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(log).To(Equal(logData))
 	})
+
+	It("should return event contents successfully", func() {
+		events, err := fetcher.FetchEvents(ctx, "default")
+
+		fmt.Printf("Fetched events: %+v\n", events)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(events).To(Equal(eventData))
+	})
+
 })
